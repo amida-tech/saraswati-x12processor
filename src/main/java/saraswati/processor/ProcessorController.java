ca5ded77664ef59072a539f5342c6e69109e803f
Francis <fkato1@umbc.edu>
2021-01-22 16:20:22 -0500

    adjustments to edi 837

--- src/main/java/saraswati/processor/ProcessorController.java
+++ src/main/java/saraswati/processor/ProcessorController.java
@@ -15,12 +15,36 @@ import com.imsweb.x12.*;
 public class ProcessorController {
  
     
-    @StreamListener(Processor.INPUT)
-    @SendTo(Processor.OUTPUT)
-    public void receive(@Payload byte[] bytes) throws Exception{
-        String message = new String(bytes, StandardCharsets.UTF_8);
-        File filename = new File(message);
-        parseEdi(filename);
+    public Segment parseSegment(String line){
+        
+        Segment segment = new Segment();
+        String [] elementStr = line.split("*", 10);
+        int count = 0;
+
+        //Add individual elements and subelements to segment
+        for(String eleStr : elementStr){
+            Element element = new Element(elementStr[count], elementStr[count+1]);
+            segment.addElement(element);
+            count++;
+        }        
+
+        return segment;
+    }
+
+    public Loop parseLoop(String current, File currentFile) throws IOException{
+
+        Loop loop = new Loop();
+        BufferedReader br = new BufferedReader(new FileReader(currentFile));
+
+        //Add segments to the loop, call recursively for inner loops
+        for(String line = current; current != null; current = br.readLine()){
+            loop.addSegment(parseSegment(line));
+            if(line.contains("HL")||line.contains("NM1")){
+                parseLoop(current, currentFile);
+            }
+        }
+
+        return loop;
     }
 
     public void parseEdi(File filename) throws IOException{
@@ -29,21 +53,27 @@ public class ProcessorController {
         X12 x12 = new X12();
         int count = 0;
 
-        for( String line = br.readLine(); line != null; line = br.readLine()){
-            
-            Loop loop = new Loop();
-            String str = line.substring(0, 2);
-            if(str.contains("HL") || str.contains("NM1")){
-                Segment segment = new Segment();
-                segment.addElements(line);
-                loop.addSegment(segment);
+        for( String line = br.readLine(); line != null; line = br.readLine()){    
+            if(line.contains("HL") || line.contains("NM1")){
+                x12.addLoop(count, parseLoop(line, filename));
+                count++;
+            } else {    
+                x12.addSegment(parseSegment(line));
                 count++;
             }
-            
-            Segment segment = new Segment();
-            segment.addElements(line);
-            x12.addSegment(segment);
-            x12.addLoop(count, loop);
         }
+
     }
+
+    @StreamListener(Processor.INPUT)
+    @SendTo(Processor.OUTPUT)
+    public void receive(@Payload byte[] bytes) throws Exception{
+        String message = new String(bytes, StandardCharsets.UTF_8);
+        File filename = new File(message);
+        parseEdi(filename);
+    }
+
+    
+
+    
 }
